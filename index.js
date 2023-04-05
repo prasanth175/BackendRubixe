c508da124a169fa8fa9181ae552f653e6c0aae25
Prasanth <1022prasanthkumar@gmail.com>
2023-04-05 20:29:15 +0530

    backend updated

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const process = require('process');
+const fetch = require('node-fetch');
+const Razorpay = require("razorpay");
+const nodemailer = require('nodemailer');
 const path = require("path");
 const cors = require('cors')
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const Table = require('cli-table');
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
 app.use(express.json());
 app.use(cors())
-
 module.exports = app;
 
 const dbPath = path.join(__dirname, "userData.db");
@@ -22,6 +25,7 @@ const initializeDBAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
+
     app.listen(3006, () => {
       console.log("Server Running at http://localhost:3006/");
     });
@@ -62,26 +66,18 @@ const authProfile = (req, res, next) => {
 };
 
 app.post("/register/", async (request, response) => {
-  const { username, password, email, mobile } = request.body;
+  const { username, password, email } = request.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const selectMobileQuery = `SELECT * FROM user WHERE mobile = '${mobile}'`;
-  const dbMobile = await db.get(selectMobileQuery);
-  if (dbMobile === undefined) {
+  const selectEmailQuery = `SELECT * FROM user WHERE email = '${email}'`;
+  const dbEmail = await db.get(selectEmailQuery);
+  if (dbEmail === undefined) {
     const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
     const dbUser = await db.get(selectUserQuery);
-    console.log(dbUser)
     if(dbUser === undefined){
-      console.log('hey')
       const createUserQuery = `
       INSERT INTO 
-        user (username, password, email, mobile) 
-      VALUES 
-        ( 
-          '${username}',
-          '${hashedPassword}', 
-          '${email}',
-          '${mobile}'
-        )`;
+        user (username, email, password) 
+      VALUES ( '${username}','${email}', '${hashedPassword}')`
     const dbResponse = await db.run(createUserQuery);
     const newUserId = dbResponse.lastID;
     const payload = {
@@ -91,17 +87,15 @@ app.post("/register/", async (request, response) => {
     response.send({ status_code: 200,
       error_txt: 'Account Created Successfully', jwtToken });
     }else{
-      console.log('hello')
       response.send({
         status_code: 400,
         error_txt: 'Username already exists'
       });
     }
-    
   } else {
     response.send({
       status_code: 400,
-      error_txt: 'Mobile already exists'
+      error_txt: 'Email already exists'
     });
   }
 });
@@ -113,7 +107,6 @@ app.post("/login/", async (req, res) => {
     `;
   const r1 = await db.get(forLogin);
   if (r1 === undefined) {
-    res.status(400);
     res.send({status_code: 400,
       error_msg: 'Invalid User'});
   } else {
@@ -125,88 +118,108 @@ app.post("/login/", async (req, res) => {
       const jwtToken = jwt.sign(payload, "PRASHANTH_KEY");
       res.send({ jwtToken });
     } else {
-      res.status(400);
       res.send({status_code: 400,
-      error_msg: 'Invalid Password',
+      error_msg: 'Wrong Password',
     username});
     }
   }
 });
 
-app.get('/books', async (req, res) => {
-  console.log('---------------')
-  const {search_by = '', category} = req.query
+app.get('/books', authProfile, async (req, res) => {
+  try {
+    const name = req.username
+    const {search_by = '', category} = req.query
   let getAllBooks;
   if(category === ''){
-    console.log('hi')
     getAllBooks = `
   SELECT * FROM sellBook 
-  WHERE description LIKE '%${search_by}%'
+  WHERE description LIKE '%${search_by}%' and userId NOT LIKE '${name}'
   `
   }else{
-    console.log(category)
     getAllBooks = `
   SELECT * FROM sellBook 
-  WHERE description LIKE '%${search_by}%' AND category LIKE '${category}'
+  WHERE description LIKE '%${search_by}%' AND category LIKE '${category}' and userId NOT LIKE '${name}'
   `
   }
 
-
-  const dbRes = await db.all(getAllBooks)
+  const dbRes = await db.all(getAllBooks, )
   res.send({dbRes})
+  } catch (error) {
+    res.status(500)
+  }
 })
 
 app.get('/books/:id', async (req, res) => {
   const bookId = req.params.id
   const getBookDetails = `
-  select * from sellBook where bookId = ${bookId}
+  select * from sellBook where bookId = '${bookId}'
   `
-
   const dbRes = await db.get(getBookDetails)
   res.send({dbRes})
 })
 
 app.get('/products/:id', async (req, res) => {
   const bookId = req.params.id
-  const getBookDetails = `
-  select * from sellBook where bookId = ${bookId}
-  `
+  const getBookDetails = 'select * from sellBook where bookId = ?'
 
-  const dbRes = await db.get(getBookDetails)
+  const dbRes = await db.get(getBookDetails, [bookId])
   res.send({dbRes})
 })
 
+app.delete('/products/:id', async (req, res) => {
+  const {id} = req.params
+  const delItem = `
+  DELETE FROM sellBook WHERE bookId = '${id}'
+  `
+
+  const dbRes = await db.run(delItem)
+  res.send({dbRes})
+})
 
 app.get('/details', authProfile, async (req, res) => {
   const name = req.username
   res.send({name})
 })
 
+app.post('/mail-details', async (req,res) => {
+  const {username} = req.body 
+  const getMail = `SELECT email FROM user WHERE username = '${username}'`
+  const dbRes = await db.get(getMail)
+  res.send({dbRes})
+})
+
 app.get('/products', authProfile, async (req, res) => {
-  const name = req.username
-  console.log(name)
+  try {
+    const name = req.username
   const getOwnBooks = `
   select * from sellBook where userId='${name}'
   `
   const dbResponse = await db.all(getOwnBooks)
-  console.log(dbResponse)
   res.send({dbResponse})
+  } catch (error) {
+    res.status(500)
+  }
 })
 
 app.post('/sell/',authProfile, async (request, response) => {
-  const name = request.username
-  console.log(name)
+  try {
+    const name = request.username
   const {category, title,
     author,
     description,
     publication_year,
     isbn,
     printed_price,
-    selling_price, language, file} = request.body;
-    const InsertData = `
-    INSERT INTO sellBook (category, title, author, description,
+    selling_price, language, file, bookId} = request.body;
+    const getBookDetails = `SELECT * FROM sellBook WHERE isbn = '${isbn}' and userId = '${name}'`
+
+    const dbGetData = await db.get(getBookDetails)
+    if(dbGetData === undefined){
+      const InsertData = `
+    INSERT INTO sellBook (bookId, category, title, author, description,
       publication_year, isbn, printed_price, selling_price, language, userId, file) 
     VALUES (
+      '${bookId}',
       '${category}',
       '${title}',
       '${author}',
@@ -220,35 +233,72 @@ app.post('/sell/',authProfile, async (request, response) => {
       '${file}'
     );
     `;
-
     const resData = await db.run(InsertData)
-    console.log(resData)
-    const bookId = resData.lastID;
-    const updateData = `
-    UPDATE sellBook SET bookId = ${bookId} WHERE bookId IS NULL`
-    
-    const anotherRes = await db.run(updateData)
-    console.log(anotherRes)
     response.send({
-      status_code: 200,
-      bookId
+      status: 200,
+      bookId,
+      message: 'Book Added Successfully'
     })
+    }else{
+      response.send({message: 'This Book is already published by you', status: 400})
+    }
+  } catch (error) {
+    console.log(error)
+    res.send({error})
+  }
 
 
 })
 
+app.post('/update-sell', async (request, response) => {
+  try {
+    const name = request.username
+  const {category, title,
+    author,
+    description,
+    publication_year,
+    isbn,
+    printed_price,
+    selling_price, language, file, bookId} = request.body;
+      const updateData = `
+    UPDATE sellBook
+    SET category = '${category}',
+    title = '${title}',
+    author= '${author}',
+    description = '${description}',
+    publication_year = '${publication_year}',
+    isbn = '${isbn}',
+    printed_price = '${printed_price}',
+    selling_price = '${selling_price}',
+    language = '${language}',
+    file = '${file}'
+    WHERE bookId = '${bookId}'
+    `;
+    const dbRes = await db.run(updateData)
+    response.send({
+      status: 200,
+      bookId,
+      message: 'Book Updated Successfully'
+    })
+  } catch (error) {
+    res.send({error})
+  }
+})
+
 app.post('/biddetails', authProfile, async (req, res) => {
-  console.log(req.username)
   const name = req.username
-  const {bookId, bidAmount, mobile} = req.body
+  const {bookId, bidAmount, mobile, file, title, description} = req.body
 
   const setData = `
-  INSERT INTO biddetails (user, bookId, bidAmount, mobile) 
+  INSERT INTO biddetails (user, bookId, bidAmount, mobile, title, description, file) 
   VALUES (
     '${name}',
-    ${bookId},
+    '${bookId}',
     '${bidAmount}',
-    '${mobile}'
+    '${mobile}',
+    '${title}',
+    '${description}',
+    '${file}'
   )
   `
 
@@ -260,10 +310,201 @@ app.post('/biddetails', authProfile, async (req, res) => {
 app.post('/biddata', async (req, res) => {
   const {bookId} = req.body 
    const getBidDetails = `
-   SELECT * FROM biddetails where bookId = ${bookId}
+   SELECT * FROM biddetails where bookId = '${bookId}'
    `
-
    const dbRes = await db.all(getBidDetails)
-   console.log(dbRes)
    res.send({dbRes})
+})
+
+app.post('/book-bid-details', authProfile, async (req, res) => {
+  const name = req.username
+  const { bookId} = req.body 
+  const getUserBids = `select * from  bidDetails where user = '${name}' and bookId = '${bookId}'`
+  const response = await db.get(getUserBids)
+  res.send({response})
+})
+
+app.get('/cart-details', authProfile, async (req, res) => {
+  try {
+    const name = req.username
+  const getCartItems = `select * from bidDetails where user = '${name}'`
+  const response = await db.all(getCartItems)
+  res.send({response})
+  } catch (error) {
+    res.status(500)
+  }
+})
+
+app.delete('/cart-details/:id', async (req, res) => {
+  const {id} = req.params
+  const delCartItem = `DELETE FROM bidDetails WHERE bookId = '${id}'`
+  const dbRes = await db.run(delCartItem)
+  res.send({dbRes})
+
+})
+
+app.post('/generate-otp', async (req, res) => {
+  const { email } = req.body;
+  const delDetails = `delete from otpDetails where email='${email}'`
+  const resp = await db.run(delDetails)
+  const otp = Math.floor(100000 + Math.random() * 900000); // generate 6-digit OTP
+  const InsertData = `INSERT INTO otpDetails (email, otp) VALUES (
+    '${email}',
+    ${otp}
+  )`
+  const response = await db.run(InsertData, (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  })
+    // send OTP to user's email
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: '1022prasanthkumar@gmail.com',
+        pass: 'uwwi sogs whpa kgxm'
+      }
+    });
+    const mailOptions = {
+      from: '1022prasanthkumar@gmail.com',
+      to: email,
+      subject: 'OTP for registration',
+      text: `Your OTP for registration is ${otp}. Please enter this OTP on the registration page
+       to verify your email address.`
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+      // console.log('OTP sent: ' + info.response);
+      res.json({ message: 'OTP generated and sent to your email address.' });
+    });
+});
+
+app.post('/verify-otp', async(req, res) => {
+  const {email, otp} = req.body
+  const verifyData = `Select * from otpDetails where email = '${email}'`
+
+  const dbRes = await db.get(verifyData, (err) => {
+    res.json({ message: 'Internal Server Problem', status: 400  });
+  })
+
+  if(dbRes === undefined){
+    res.json({ message: 'Email Not Found', status: 401 });
+  }else{
+    const verifyOtp = dbRes.otp === otp
+    if(!verifyOtp){
+      res.json({ message: 'Invalid OTP', status: 402  });
+    }else{
+      res.json({ message: 'Email Verified', status: 200  });
+
+    }
+  }
+})
+
+// Integrate Razor pay Into our project
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_SUCPQkxTXgHfQb",
+  key_secret: "GEUQlYKXrkQbt3Qh6IIzg5SO",
+});
+
+app.post('/create-order', async (req, res) => {
+  const { amount, currency } = req.body;
+
+  const options = {
+    amount: amount,
+    currency: currency,
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (error) {
+    res.status(500).send("Error creating order");
+  }
+});
+
+app.get('/payments/:id', async (req, res) => {
+  const {id} = req.params
+  const getDetails = `SELECT * FROM sellBook WHERE bookId = '${id}'`
+  const dbRes = await db.get(getDetails)
+  res.send({dbRes})
+})
+
+app.post('/set-password', async (req,res)=> {
+  try {
+    const {password, email} = req.body 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const getEmail = `SELECT * FROM user WHERE email = '${email}'`
+
+  const dbResponse = await db.get(getEmail)
+  if(dbResponse === undefined){
+    res.send({message: 'No user is registered with this Email', status: 400})
+  }else{
+    const updateData = `
+    UPDATE user 
+    SET password = '${hashedPassword}'
+    WHERE email = '${email}'
+    `
+    const dbRes = await db.run(updateData)
+    res.send({message: 'Password Successfully Updated', status: 200})
+  }
+  } catch (error) {
+    res.send({message: error, status: 500})
+  }
+})
+
+app.post('/change-password',authProfile, async (req,res)=> {
+  try {
+   const passDetails = req.body
+   const {currentPassword, confirmPassword} = passDetails
+   const name = req.username
+   const getPassword = `SELECT password FROM user WHERE username = '${name}'`
+   const dbRes = await db.get(getPassword)
+   const isCorrect = await bcrypt.compare(dbRes.password, currentPassword)
+   if(isCorrect){
+    const hashedPassword = await bcrypt.hash(confirmPassword, 10)
+    const updatePass = `
+    UPDATE user 
+    SET password = '${hashedPassword}'
+    WHERE username = '${name}'
+    `
+
+    const dbResponse = await db.run(updatePass)
+    res.send({
+      error_msg: 'Password Updated Successfully',
+      error_code: 200
+    })
+   }else{
+    res.send({
+      error_msg: 'Incorrect Password',
+      error_code: 400
+    })
+   }
+  }catch (error) {
+    console.log(error)
+    res.send({message: error, status: 500})
+  }
+})
+
+app.post('/proxy/razorpay', async (req, res) => {
+  try {
+    const { url, method, headers } = req.body;
+    const response = await fetch(url, {
+      method,
+      headers,
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/userDetails', async (req, res) => {
+  const getDetails = `pragma table_info(user)`
+  const dbRes = await db.all(getDetails)
+  res.send({dbRes})
 })
